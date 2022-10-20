c3e1129b2907b5b4385f724c8dfe1774f1937bfd
Karen Heredia <karenherediadg@gmail.com>
2022-10-19 19:58:17 -0500

    input errors

--- src/app/pages/registro/registro.component.ts
+++ src/app/pages/registro/registro.component.ts
@@ -15,8 +15,6 @@ export class RegistroComponent implements OnInit {
 
   ngOnInit() { 
     this.usuario = new UsuarioModel();
-
-    this.usuario.email = 'abc@suki.com';
   }
 
   onSubmit(form: NgForm) {
